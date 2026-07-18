9e1929aa78a3cae067ee740e96ee187076402eb2
Mikayıl Quliyev <mikayilquliyev16@gmail.com>
2026-07-18 15:08:17 +0400

    feat: implement author service

---
+++ src/main/java/librarymanagement/service/AuthorService.java
@@ -0,0 +1,56 @@
+package librarymanagement.service;
+
+import librarymanagement.dto.request.AuthorRequest;
+import librarymanagement.dto.response.AuthorResponse;
+import librarymanagement.entity.Author;
+import librarymanagement.mapper.AuthorMapper;
+import librarymanagement.repository.AuthorRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AuthorService {
+
+    private final AuthorRepository authorRepository;
+    private final AuthorMapper authorMapper;
+
+    public AuthorResponse create(AuthorRequest request) {
+        Author author = authorMapper.toEntity(request);
+        Author savedAuthor = authorRepository.save(author);
+        return authorMapper.toResponse(savedAuthor);
+    }
+
+    public List<AuthorResponse> getAll() {
+        return authorMapper.toResponseList(authorRepository.findAll());
+    }
+
+    public AuthorResponse getById(Long id) {
+        Author author = authorRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Author not found"));
+
+        return authorMapper.toResponse(author);
+    }
+
+    public AuthorResponse update(Long id, AuthorRequest request) {
+        Author author = authorRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Author not found"));
+
+        author.setFirstName(request.getFirstName());
+        author.setLastName(request.getLastName());
+        author.setEmail(request.getEmail());
+
+        Author updatedAuthor = authorRepository.save(author);
+
+        return authorMapper.toResponse(updatedAuthor);
+    }
+
+    public void delete(Long id) {
+        Author author = authorRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Author not found"));
+
+        authorRepository.delete(author);
+    }
+}
